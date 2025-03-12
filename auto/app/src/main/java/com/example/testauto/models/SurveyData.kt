8ee3db0a8dc3560e59555f501b60aa8ec5a77017
YaroslavNasonov <yaroslav.nas@mail.ru>
2025-03-12 16:34:16 +0300

    add SurveyData

---
+++ auto/app/src/main/java/com/example/testauto/models/SurveyData.kt
@@ -0,0 +1,10 @@
+package com.example.testauto.models
+
+data class SurveyData(
+    val name: String,
+    val avgKm: String,
+    val brand: String,
+    val model: String,
+    val year: String,
+    val mileage: String
+)
