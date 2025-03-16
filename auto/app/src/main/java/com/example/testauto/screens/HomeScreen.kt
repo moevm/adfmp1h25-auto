5dbb13e0425ea11fc54eb501362f849d3374dfbe
Arthur Ivanov <arthur.commercial@mail.ru>
2025-03-16 19:23:10 +0300

    add: added packages and vars to home screen and preparing fow main func

--- auto/app/src/main/java/com/example/testauto/screens/HomeScreen.kt
+++ auto/app/src/main/java/com/example/testauto/screens/HomeScreen.kt
@@ -1,25 +1,44 @@
 package com.example.testauto.screens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.testauto.components.CustomTopBar
 import com.example.testauto.components.InfoDialog
+import com.example.testauto.models.MaintenanceLog
+import com.example.testauto.models.Reminder
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
 @Composable
 fun HomeScreen(
     navController: NavHostController,
+    reminders: List<Reminder>,
+    maintenanceLogs: List<MaintenanceLog>
 ) {
     var showInfo by remember { mutableStateOf(false) }
     if (showInfo) {
         InfoDialog { showInfo = false }
     }
+
+    val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+    val nearestReminder = reminders.minByOrNull { LocalDate.parse(it.repairDate, formatter) }
+    val lastCheck = maintenanceLogs.maxByOrNull { LocalDate.parse(it.date, formatter) }
+
 
     Scaffold(
         topBar = {
